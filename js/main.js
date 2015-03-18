4f4bd9fa0be256dd49e988b3fb5678bfaaeecf69
valeriebordeleau <bord0044@algonquinlive.com>
2015-03-18 15:46:03 -0400

    Made text boxes collapsable
    Text boxes are now collapsable using a toggle class and an animation

--- js/main.js
+++ js/main.js
@@ -4,3 +4,24 @@ var $navItems = $('.nav-items');
 $toggle.on('click', function () {
     $navItems.toggleClass('js-toggle-appear');
 });
+
+var $thingsToDo = $('.things-to-do');
+var $toDo = $('.to-do');
+
+$thingsToDo.on('click', function () {
+    $toDo.toggleClass('js-to-do-appear');
+});
+
+var $thingsToSee = $('.things-to-see');
+var $toSee = $('.to-see');
+
+$thingsToSee.on('click', function () {
+    $toSee.toggleClass('js-to-see-appear');
+});
+
+var $thingsToGo = $('.things-to-go');
+var $toGo = $('.to-go');
+
+$thingsToGo.on('click', function () {
+    $toGo.toggleClass('js-to-go-appear');
+});
